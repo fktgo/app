4c78c76652420cd4e91c3de36b295d2d479f3b14
Rodrigo Bernardo Pimentel <rbp@isnomore.net>
2021-05-14 21:20:49 +0200

    Initial smoke widget tests!

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,18 +13,21 @@ import 'package:app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(FKTGo());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Ready?'), findsOneWidget);
+    expect(find.text('Go'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the 'Go' button
+    await tester.tap(find.byType(ElevatedButton));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Ready?'), findsNothing);
+    expect(find.text('Stop'), findsOneWidget);
+
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pump();
+    expect(find.text('Ready?'), findsOneWidget);
+    expect(find.text('Go'), findsOneWidget);
   });
 }
